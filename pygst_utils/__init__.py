c46accdf3b2947c48bc3212d58ad83d64dd81c2e
vitaliivovk <55910087+vitaliivovk@users.noreply.github.com>
2019-11-02 18:00:06 +0200

    Added gst_objects_info_metafunctions

--- pygst_utils/__init__.py
+++ pygst_utils/__init__.py
@@ -7,3 +7,4 @@ from .gst_hacks import map_gst_buffer, get_buffer_size
 from .gst_hacks import map_gst_memory
 
 from .utils import gst_buffer_to_ndarray, gst_buffer_with_pad_to_ndarray, numpy_to_gst_buffer
+from .gst_objects_info_meta import to_gst_objects_info, to_list, gst_meta_write, gst_meta_get, gst_meta_remove
